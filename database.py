fe8a105d8c0350a943adb5d0a44bee1375fba951
Alexico1969 <alexicoo@gmail.com>
2023-10-17 18:48:02 -0400

    added register and login function

---
+++ database.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+def connector():
+    conn = sqlite3.connect('database.db')
+    print("Opened database successfully")
+    conn.execute('CREATE TABLE IF NOT EXISTS users (name TEXT, email TEXT, username TEXT, password TEXT, level INTEGER, score INTEGER, inventory TEXT )')
+    print("Table 'users' created successfully")
+    conn.close()
+    
+def check_login(username, password):
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
+    data = c.fetchall()
+    conn.close()
+    if len(data) == 0:
+        return False
+    return True
+
+def get_user(username):
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM users WHERE username = ?", (username,))
+    data = c.fetchall()
+    conn.close()
+    return data
+
+def register_user(name, email, username, password, level, score, inventory):
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO users (name, email, username, password, level, score, inventory) VALUES (?, ?, ?, ?, ?, ?, ?)", (name, email, username, password, level, score, inventory))
+    conn.commit()
+    conn.close()
+
+
+# room variables: level : [furneture, inventory, type, keywords, output, info]
+rooms = {
+    1: ["1", "table,chair" , "key", "inventory", "key", "", ""],
+    2: ["2", "table,computer,chair", "code", "print,Hello,World", "Hello World", ""],
+}
--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,68 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
-
+from flask_session import Session
+from database import connector, check_login, register_user, rooms
 
 app = Flask(__name__)
 
-@app.route('/')
+app.config['SECRET_KEY'] = 'your-secret-key-123'
+app.config['SESSION_TYPE'] = 'filesystem'
+
+db = connector()
+print(db)
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if 'user' not in session:
+            return redirect(url_for('login'))
+    
     return render_template('home.html')
 
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+
+        # Check if the username and password are valid
+
+        if check_login(username, password):
+            flash('Username already exists')
+            return redirect(url_for('register'))
+
+        # Register the user
+        register_user(name, email, username, password, 1, 0, "")
+
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Check if the username and password are valid
+
+        if check_login(username, password):
+            session['user'] = username
+            return redirect(url_for('home'))
+
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('home'))
+
+
+# -------------------------------------------------------------------------------------------
 
 if __name__ == '__main__':
     app.run(debug=True)
